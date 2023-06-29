cf5afae05522da4dbeef443588f68d36922ca8ed
Özkan Ardil <ozkanardil@gmail.com>
2023-06-29 08:51:59 +0300

    WebAPI project to read data from registry has been completed

---
+++ src/RegistryApp.Read/Controllers/RegistryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RegistryApp.BLL.Managers;
+using RegistryApp.BLL.Security;
+
+namespace RegistryApp.Read.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            ReadManager readManager = new ReadManager();
+            var encryptedText = readManager.GetValueFromRegistry();
+
+            DencryptData decryptText = new DencryptData();
+            string result = decryptText.Decrypt(encryptedText);
+
+            return Ok(result);
+        }
+    }
+}
