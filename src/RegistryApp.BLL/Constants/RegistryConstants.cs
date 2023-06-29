0f68ab81aa6ec2f0c12341fb9521135e4c0cc35c
Özkan Ardil <ozkanardil@gmail.com>
2023-06-29 08:50:47 +0300

    Registry constants class has been created

---
+++ src/RegistryApp.BLL/Constants/RegistryConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace RegistryApp.BLL.Constants
+{
+    public static class RegistryConstants
+    {
+        public static readonly string SecurityKey = "ThisKyIsSecurityKey";
+        public static readonly string RegistryDirectory = "SOFTWARE";
+        public static readonly string RegistryTag = "Test";
+        public static readonly string RegistryVariable = "MyKey";
+    }
+}
