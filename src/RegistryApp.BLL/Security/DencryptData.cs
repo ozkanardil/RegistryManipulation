a6ee58036bc89ef28f27243b095278d51dcc32c3
Özkan Ardil <ozkanardil@gmail.com>
2023-06-29 08:51:31 +0300

    Encryption and decryption classes have been created

---
+++ src/RegistryApp.BLL/Security/DencryptData.cs
@@ -0,0 +1,30 @@
+﻿using RegistryApp.BLL.Constants;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RegistryApp.BLL.Security
+{
+    public class DencryptData
+    {
+        public string Decrypt(string cipherText)
+        {
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (System.Security.Cryptography.Aes encyptor = System.Security.Cryptography.Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(RegistryConstants.SecurityKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encyptor.Key = pdb.GetBytes(32);
+                encyptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encyptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+    }
+}
---
+++ src/RegistryApp.BLL/Security/EncryptData.cs
@@ -0,0 +1,30 @@
+﻿using RegistryApp.BLL.Constants;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RegistryApp.BLL.Security
+{
+    public class EncryptData
+    {
+        public string Encrypt(string clearText)
+        {
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (System.Security.Cryptography.Aes encyptor = System.Security.Cryptography.Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(RegistryConstants.SecurityKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encyptor.Key = pdb.GetBytes(32);
+                encyptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encyptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+    }
+}
