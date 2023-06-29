bdee55665552fba405ac63c6bc8f0fdf603b9b31
Özkan Ardil <ozkanardil@gmail.com>
2023-06-29 08:51:08 +0300

    Read and write manager classes have been created

---
+++ src/RegistryApp.BLL/Managers/ReadManager.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using RegistryApp.BLL.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistryApp.BLL.Managers
+{
+    public class ReadManager
+    {
+        public string GetValueFromRegistry()
+        {
+            var registryKey = Registry.LocalMachine
+                                .OpenSubKey(RegistryConstants.RegistryDirectory)
+                                .OpenSubKey(RegistryConstants.RegistryTag);
+
+            string regKey = registryKey.GetValue(RegistryConstants.RegistryVariable).ToString();
+            return regKey;
+        }
+
+
+    }
+}
---
+++ src/RegistryApp.BLL/Managers/WriteManager.cs
@@ -0,0 +1,32 @@
+﻿using RegistryApp.BLL.Constants;
+using System.Security.AccessControl;
+
+namespace RegistryApp.BLL.Managers
+{
+    public class WriteManager
+    {
+        public bool CreateRegistryValue(string regKey, string regValue)
+        {
+            bool result = false;
+
+            try
+            {
+                string user = Environment.UserDomainName + "\\" + Environment.UserName;
+                RegistryAccessRule rule = new RegistryAccessRule(user, RegistryRights.FullControl, AccessControlType.Allow);
+                RegistrySecurity security = new RegistrySecurity();
+                security.AddAccessRule(rule);
+                string path = string.Format("{0}\\{1}", RegistryConstants.RegistryDirectory, RegistryConstants.RegistryTag);
+                var key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(path, true);
+                key.SetAccessControl(security);
+                key.SetValue(regKey, regValue);
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Message: " + ex.Message);
+                Console.WriteLine("Inner Exception: " + ex.InnerException);
+            }
+            return result;
+        }
+    }
+}
